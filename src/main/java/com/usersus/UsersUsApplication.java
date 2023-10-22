e687a5b91c227d5c4cd1d66a9bd7f99a32b1f16e
Radu-Augustin Vele <vele.ne.radu@student.utcluj.ro>
2023-10-22 16:20:42 +0300

    initial commit -blank spring project + properties example

---
+++ src/main/java/com/usersus/UsersUsApplication.java
@@ -0,0 +1,13 @@
+package com.usersus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersUsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usersus/UsersUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usersus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
